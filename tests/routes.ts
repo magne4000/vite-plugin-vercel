7a443967d4f7de5e4d50504c6c16779fb2dc10bd
Joël Charles <joel.charles91@gmail.com>
2022-03-17 17:53:21 +0100

    test: prepare test scenario

--- tests/routes.ts
+++ tests/routes.ts
@@ -1,5 +1,11 @@
 import fs from 'fs/promises';
-import type { RoutesManifest } from 'vite-plugin-vercel';
+import { routesManifestSchema } from 'vite-plugin-vercel/src/schemas/manifests/routes';
+import myzod from 'myzod';
+
+// TODO create tests scenarios
+// 1. execute scenario that generates .output
+// 2. execute all tests related to this scenario
+// 3. if successful, execute next scenario
 
 describe('routes-manifest.json - pre tests', function () {
   it('should exist in .output/routes-manifest.json', async function () {
@@ -8,39 +14,13 @@ describe('routes-manifest.json - pre tests', function () {
     expect(stats.isFile()).toBe(true);
   });
 
-  it('should be JSON format', async function () {
+  it('should respect schema', async function () {
     const manifest = await fs.readFile('.output/routes-manifest.json', {
       encoding: 'utf-8',
     });
 
-    expect(JSON.parse(manifest)).toMatchObject({});
-  });
-});
-
-describe('routes-manifest.json', function () {
-  let manifest: RoutesManifest;
-
-  beforeAll(async () => {
-    manifest = JSON.parse(
-      await fs.readFile('.output/routes-manifest.json', {
-        encoding: 'utf-8',
-      }),
+    expect(routesManifestSchema.try(JSON.parse(manifest))).not.toBeInstanceOf(
+      myzod.ValidationError,
     );
   });
-
-  it('should be at version 3', function () {
-    expect(manifest).toHaveProperty('version', 3);
-  });
-
-  it('should have a basePath string value', function () {
-    expect(manifest).toHaveProperty('basePath');
-    expect(typeof manifest.basePath).toBe('string');
-  });
-
-  it('should have a pages404 boolean value', function () {
-    expect(manifest).toHaveProperty('pages404');
-    expect(typeof manifest.pages404).toBe('boolean');
-  });
-
-  it('should not have unknow properties', function () {});
 });
