35c4d30fa54a1a9d46dce951032fdb82d684aa0b
Joël Charles <joel.charles91@gmail.com>
2022-05-17 13:25:51 +0200

    test

--- packages/vercel/src/index.ts
+++ packages/vercel/src/index.ts
@@ -51,7 +51,8 @@ function vercelPlugin(): Plugin {
 
       // step 3.4:	Generate config file
       await writeConfig(resolvedConfig, {
-        routes: [{ handle: 'filesystem' }, ...rewrites],
+        // routes: [{ handle: 'filesystem' }, ...rewrites],
+        routes: [...rewrites],
         overrides,
       });
     },
