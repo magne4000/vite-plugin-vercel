47b58237427745a8e9a5d624fbe5c9bc117041da
Joël Charles <joel.charles91@gmail.com>
2022-05-20 16:55:09 +0200

    chore: change servless target back to cjs

--- packages/vercel/src/build.ts
+++ packages/vercel/src/build.ts
@@ -43,7 +43,7 @@ export function getEntries(
 const standardBuildOptions: BuildOptions = {
   bundle: true,
   target: 'es2020',
-  format: 'esm',
+  format: 'cjs',
   platform: 'node',
   logLevel: 'info',
   minify: true,
