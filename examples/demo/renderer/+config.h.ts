ef0da7d4e882f751f0d8c7e1746fb644ac4680a6
Joël Charles <joel.charles91@gmail.com>
2023-09-07 15:01:23 +0200

    new demo

---
+++ examples/demo/renderer/+config.h.ts
@@ -0,0 +1,37 @@
+import type { Config } from 'vite-plugin-ssr/types';
+import config from '@vite-plugin-vercel/vike/config';
+
+// https://vite-plugin-ssr.com/config
+export default {
+  passToClient: ['pageProps', 'title', 'someAsyncProps'],
+  clientRouting: true,
+  hydrationCanBeAborted: true,
+  // https://vite-plugin-ssr.com/meta
+  meta: {
+    // Create new config 'title'
+    title: {
+      env: 'server-and-client',
+    },
+    // Create new config 'onBeforeRenderIsomorph'
+    onBeforeRenderIsomorph: {
+      env: 'config-only',
+      effect({ configDefinedAt, configValue }) {
+        if (typeof configValue !== 'boolean') {
+          throw new Error(`${configDefinedAt} should be a boolean`);
+        }
+        if (configValue) {
+          return {
+            meta: {
+              onBeforeRender: {
+                // We override VPS's default behavior of always loading/executing onBeforeRender() on the server-side.
+                // If we set onBeforeRenderIsomorph to true, then onBeforeRender() is loaded/executed in the browser as well, allowing us to fetch data direcly from the browser upon client-side navigation (without involving our Node.js/Edge server at all).
+                env: 'server-and-client',
+              },
+            },
+          };
+        }
+      },
+    },
+  },
+  extends: config,
+} satisfies Config;
