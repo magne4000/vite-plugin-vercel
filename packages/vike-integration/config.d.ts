9fb92087bcac3a21a1dcfeb8a318d5e6b42adbb7
Joël Charles <joel.charles91@gmail.com>
2023-09-16 12:53:34 +0200

    chore: update vps version

--- packages/vike-integration/config.d.ts
+++ packages/vike-integration/config.d.ts
@@ -3,9 +3,11 @@ import 'vite-plugin-ssr/types';
 // When this is imported by a projet ->
 //   import config from '@vite-plugin-vercel/vike/config'
 // the following override is applied to the whole project
-declare module 'vite-plugin-ssr/types' {
-  export interface ConfigVikePackages {
-    isr?: boolean | { expiration: number };
+declare global {
+  namespace Vike {
+    export interface Config {
+      isr?: boolean | { expiration: number };
+    }
   }
 }
 
