ff93906156bd95634b1759d438e9586b60ad12ca
Joël Charles <joel.charles91@gmail.com>
2023-09-16 12:13:39 +0200

    chore: update simple example api usage

--- examples/simple/vite.config.ts
+++ examples/simple/vite.config.ts
@@ -4,6 +4,8 @@ import vercel from 'vite-plugin-vercel';
 export default defineConfig({
   plugins: [
     vercel({
+      // `smart` param only exist to circumvent a pnpm issue in this repo
+      // You should not use this parameter outside this repository
       smart: false,
     }),
   ],
