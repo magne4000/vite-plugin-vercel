b536e8b39fcf1418eb7aa825354af526c62a9c9c
Joël Charles <joel.charles91@gmail.com>
2024-03-18 16:28:10 +0100

    chore: update layout

--- examples/demo/renderer/PageShell.tsx
+++ examples/demo/renderer/PageShell.tsx
@@ -27,7 +27,8 @@ function PageShell({
             <Link href="/catch-all/a/b/c">Catch-all</Link>
             <Link href="/function/a">Function</Link>
             <Link href="/edge">Edge Function endpoint</Link>
-            <Link href="/og">Edge OG endpoint</Link>
+            <Link href="/og-edge">Edge OG endpoint</Link>
+            <Link href="/og-node">Node OG endpoint</Link>
           </Sidebar>
           <Content>{children}</Content>
         </Layout>
