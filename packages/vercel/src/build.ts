c9d66558ca9150100ac90c5938d0f5bf2871aaab
Joël Charles <joel.charles91@gmail.com>
2024-03-22 10:18:49 +0100

    fix: namespace JS banner in order to minimize potential declarations conflict

--- packages/vercel/src/build.ts
+++ packages/vercel/src/build.ts
@@ -162,12 +162,12 @@ export async function buildFn(
     options.format = 'esm';
   } else if (options.format === 'esm') {
     options.banner = {
-      js: `import { createRequire } from 'node:module';
-import path from 'node:path';
-import url from 'node:url';
-const require = createRequire(import.meta.url);
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+      js: `import { createRequire as VPV_createRequire } from "node:module";
+import { fileURLToPath as VPV_fileURLToPath } from "node:url";
+import { dirname as VPV_dirname } from "node:path";
+const require = VPV_createRequire(import.meta.url);
+const __filename = VPV_fileURLToPath(import.meta.url);
+const __dirname = VPV_dirname(__filename);
 `,
     };
   }
