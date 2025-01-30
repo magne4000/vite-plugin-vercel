b1a88a499d23ed06375c6dd06b84a6b472902b82
Joël Charles <joel.charles91@gmail.com>
2025-01-30 17:56:03 +0100

    chore

--- examples/hono/vite.config.ts
+++ examples/hono/vite.config.ts
@@ -1,10 +1,8 @@
-import react from "@vitejs/plugin-react-swc";
 import type { UserConfig } from "vite";
 import vercel from "vite-plugin-vercel";
 
 export default {
   plugins: [
-    react(),
     vercel({
       entries: [
         {
