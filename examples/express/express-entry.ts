d8a3ba89c5c636afc27ab162a374c09e4bdcb9b6
Joël Charles <joel.charles91@gmail.com>
2025-01-30 15:24:56 +0100

    refactor: express example

--- examples/express/express-entry.ts
+++ examples/express/express-entry.ts
@@ -1,9 +1,8 @@
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
-import { createHandler, createMiddleware } from "@universal-middleware/express";
+import { createHandler } from "@universal-middleware/express";
 import express from "express";
-import { vikeHandler } from "./server/vike-handler";
 
 const __filename = globalThis.__filename ?? fileURLToPath(import.meta.url);
 const __dirname = globalThis.__dirname ?? dirname(__filename);
@@ -16,9 +15,7 @@ export default await startServer();
 async function startServer() {
   const app = express();
 
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(`${root}/dist/client`));
-  } else {
+  if (process.env.NODE_ENV !== "production") {
     // Instantiate Vite's development server and integrate its middleware to our server.
     // ! We should instantiate it *only* in development. (It isn't needed in production
     // and would unnecessarily bloat our server in production.)
@@ -32,20 +29,12 @@ async function startServer() {
     app.use(viteDevMiddleware);
   }
 
-  app.get(
-    "/hello",
-    createMiddleware(() => () => {
-      console.log("HELLO");
-      return new Response("Hello");
+  app.all(
+    "*",
+    createHandler(() => () => {
+      return new Response("OK");
     })(),
   );
-
-  /**
-   * Vike route
-   *
-   * @link {@see https://vike.dev}
-   **/
-  app.all("*", createHandler(() => vikeHandler)());
 
   app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`);
