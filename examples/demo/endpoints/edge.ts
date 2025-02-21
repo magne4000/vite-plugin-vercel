ec94151ace6062add9a74c46ee02fed5051eb383
Joël Charles <joel.charles91@gmail.com>
2025-02-21 21:52:23 +0100

    test: fix edge test in dev

--- examples/demo/endpoints/edge.ts
+++ examples/demo/endpoints/edge.ts
@@ -3,12 +3,14 @@ import { createClient } from "@vercel/edge-config";
 export const edge = true;
 
 export default async function handler() {
-  if (import.meta.env.NODE_ENV === "development" && !import.meta.env.EDGE_CONFIG) {
+  const EDGE_CONFIG = import.meta.env.EDGE_CONFIG ?? process.env.EDGE_CONFIG;
+
+  if (import.meta.env.NODE_ENV === "development" && !EDGE_CONFIG) {
     throw new Error(
       "Please add EDGE_CONFIG in .env.development.local. See https://vercel.com/docs/storage/edge-config/using-edge-config",
     );
   }
-  const edgeConfig = createClient(import.meta.env.EDGE_CONFIG);
+  const edgeConfig = createClient(EDGE_CONFIG);
   await edgeConfig.get("someKey");
 
   return new Response("Edge Function: OK", {
