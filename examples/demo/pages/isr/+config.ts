f562a18eb2b181eab0fd04e8a5439597bb573d5f
Romuald Brillout <git@brillout.com>
2025-02-27 17:18:32 +0100

    use latest Vike

--- examples/demo/pages/isr/+config.ts
+++ examples/demo/pages/isr/+config.ts
@@ -1,7 +1,5 @@
 import type { Config } from "vike/types";
 
 export default {
-  // FIXME: Setting ISR should disable prerendering
-  prerender: false,
   isr: { expiration: 5 },
 } satisfies Config;
