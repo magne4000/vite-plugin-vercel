ba2026df5b26e0af3e62098d1b7797ba6bd62652
Joël Charles <joel.charles91@gmail.com>
2025-03-03 11:26:01 +0100

    test: fix

--- examples/demo/e2e-tests/function.spec.ts
+++ examples/demo/e2e-tests/function.spec.ts
@@ -1,16 +1,16 @@
-import { test } from "@playwright/test";
-import { goto, isDeployed, testDates } from "./utils";
+import { test } from '@playwright/test'
+import { goto, isDeployed, testDates } from './utils'
 
-test("has static pages", async ({ page }) => {
-  test.skip(!isDeployed);
-  await goto(page, "/catch-all/a");
-  await testDates(page, "same");
-});
+test('has static pages', async ({ page }) => {
+  test.skip(!isDeployed)
+  await goto(page, '/function/a')
+  await testDates(page, 'same')
+})
 
-test("has dynamic pages", async ({ page }) => {
-  await goto(page, "/catch-all/a");
-  await testDates(page, "newer");
+test('has dynamic pages', async ({ page }) => {
+  await goto(page, '/function/a')
+  await testDates(page, 'newer')
 
-  await goto(page, "/catch-all/e/f/g/h/i");
-  await testDates(page, "newer");
-});
+  await goto(page, '/function/e/f/g/h/i')
+  await testDates(page, 'newer')
+})
