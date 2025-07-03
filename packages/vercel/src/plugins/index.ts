7a307c638439f2009909dbf0020751db05cd5d32
Joël Charles <joel.charles91@gmail.com>
2025-07-03 18:54:17 +0200

    chore: format

--- packages/vercel/src/plugins/index.ts
+++ packages/vercel/src/plugins/index.ts
@@ -1,12 +1,12 @@
-import type { ViteVercelConfig } from '../types';
-import type { PluginOption } from 'vite';
-import { vercelCleanupPlugin } from './clean-outdir';
-import { wasmPlugin } from './wasm';
-import { apiPlugin } from './api';
-import { bundlePlugin } from './bundle';
-import { installPhoton } from '@photonjs/core/vite';
-import { setupEnvs } from './setupEnvs';
-import { loaderPlugin } from './loader';
+import type { ViteVercelConfig } from "../types";
+import type { PluginOption } from "vite";
+import { vercelCleanupPlugin } from "./clean-outdir";
+import { wasmPlugin } from "./wasm";
+import { apiPlugin } from "./api";
+import { bundlePlugin } from "./bundle";
+import { installPhoton } from "@photonjs/core/vite";
+import { setupEnvs } from "./setupEnvs";
+import { loaderPlugin } from "./loader";
 
 export function vercel(pluginConfig: ViteVercelConfig = {}): PluginOption[] {
   const additionalConfig: Record<string, unknown> = {};
