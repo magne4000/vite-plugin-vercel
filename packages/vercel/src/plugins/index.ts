a8d3813ae77284aa622341600289ab99fb64ff6e
Joël Charles <joel.charles91@gmail.com>
2025-07-03 18:53:52 +0200

    chore

--- packages/vercel/src/plugins/index.ts
+++ packages/vercel/src/plugins/index.ts
@@ -1,12 +1,12 @@
-import type { ViteVercelConfig } from "../types";
-import type { PluginOption } from "vite";
-import { vercelCleanupPlugin } from "./clean-outdir";
-import { wasmPlugin } from "./wasm";
-import { apiPlugin } from "./api";
-import { bundlePlugin } from "./bundle";
-import { installPhoton } from "@photonjs/core/vite";
-import { setupEnvs } from "./setupEnvs";
-import { loaderPlugin } from "./loader";
+import type { ViteVercelConfig } from '../types';
+import type { PluginOption } from 'vite';
+import { vercelCleanupPlugin } from './clean-outdir';
+import { wasmPlugin } from './wasm';
+import { apiPlugin } from './api';
+import { bundlePlugin } from './bundle';
+import { installPhoton } from '@photonjs/core/vite';
+import { setupEnvs } from './setupEnvs';
+import { loaderPlugin } from './loader';
 
 export function vercel(pluginConfig: ViteVercelConfig = {}): PluginOption[] {
   const additionalConfig: Record<string, unknown> = {};
@@ -20,6 +20,8 @@ export function vercel(pluginConfig: ViteVercelConfig = {}): PluginOption[] {
   return [
     ...installPhoton("vite-plugin-vercel", {
       ...additionalConfig,
+      // FIXME avoid duplicate plugins
+      fullInstall: true,
       defaultBuildEnv: "vercel_node",
       devServer: {
         env: "vercel_node",
