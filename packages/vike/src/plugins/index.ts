faf0a74265fe2bab481be70896a489ce10d691f1
Joël Charles <joel.charles91@gmail.com>
2025-08-27 15:45:03 +0200

    chore: bump vike, photon and vike-server

--- packages/vike/src/plugins/index.ts
+++ packages/vike/src/plugins/index.ts
@@ -1,8 +1,8 @@
+import { installPhoton } from "@photonjs/runtime/vite";
 import type { Plugin } from "vite";
 import { overrideConfPlugin } from "./override-conf";
-import { routesPlugins } from "./routes";
-import { installPhoton } from "@photonjs/core/vite";
 import { prerenderPlugin } from "./prerender";
+import { routesPlugins } from "./routes";
 
 export const vikeVercel: Plugin[] = [
   ...installPhoton("vike-vercel", {
