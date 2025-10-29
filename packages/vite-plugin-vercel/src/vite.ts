46327d3fafc74fd49f7986fce1d4854161f850bb
Joël Charles <joel.charles91@gmail.com>
2025-10-29 16:01:34 +0100

    chore

--- packages/vite-plugin-vercel/src/vite.ts
+++ packages/vite-plugin-vercel/src/vite.ts
@@ -1,10 +1,9 @@
-import { installPhotonCore } from "@photonjs/core/vite";
+import { installPhotonResolver } from "@photonjs/core/vite";
 import plugin from "@photonjs/vercel/vite";
 
 export const vercel: typeof plugin = (options) => {
   const plugins = plugin(options);
-  // TODO export new installPhoton util only for aliases
-  return [...plugins, ...(installPhotonCore("vite-plugin-vercel") as typeof plugins)];
+  return [...plugins, ...(installPhotonResolver("vite-plugin-vercel") as typeof plugins)];
 };
 
 export default vercel;
