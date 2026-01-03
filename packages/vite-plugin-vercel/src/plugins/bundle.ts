f976a15c4f8f875629a744b1d08d85a84741d67e
Joël Charles <joel.charles91@gmail.com>
2026-01-03 23:27:11 +0100

    chore

--- packages/vite-plugin-vercel/src/plugins/bundle.ts
+++ packages/vite-plugin-vercel/src/plugins/bundle.ts
@@ -5,8 +5,15 @@ import { wasmPlugin } from "./wasm.js";
 export function bundlePlugin() {
   return standaloner({
     external: edgeExternal,
+    isolated: true,
     bundle: {
+      output: {
+        // disable CJS banner
+        banner: "",
+      },
       plugins: [wasmPlugin()],
+      // FIXME this one is not used, but it should be the only one
+      isolated: true,
     },
   });
 }
