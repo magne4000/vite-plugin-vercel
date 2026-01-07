d018b5404a9d5c983cca726f4b03aebbb47fd89c
Joël Charles <joel.charles91@gmail.com>
2026-01-07 10:55:08 +0100

    test

--- examples/demo/e2e-tests/catch-all.spec.ts
+++ examples/demo/e2e-tests/catch-all.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "@playwright/test";
-import { goto, isDeployed, testISRDates } from "./utils";
+import { goto } from "./utils";
 
 test("catches all URLs", async ({ page }) => {
   {
@@ -11,23 +11,9 @@ test("catches all URLs", async ({ page }) => {
     const response = await goto(page, "/catch-all/a/c/d/e");
     expect(response?.status()).toBe(200);
   }
-});
 
-test("has ISR pages (static override)", async ({ page }) => {
-  test.skip(!isDeployed);
-  await goto(page, "/catch-all/a/b/c");
-  await testISRDates(page, 5000);
-
-  await goto(page, "/catch-all/a/d");
-  await testISRDates(page, 5000);
-});
-
-test("has ISR pages", async ({ page }) => {
-  test.skip(!isDeployed);
-  test.slow();
-  await goto(page, "/catch-all/c/d/e");
-  await testISRDates(page, 5000);
-
-  await goto(page, "/catch-all/f/g");
-  await testISRDates(page, 5000);
+  {
+    const response = await goto(page, "/named/test/a/b/c");
+    expect(response?.status()).toBe(200);
+  }
 });
--- examples/demo/e2e-tests/named.spec.ts
+++ examples/demo/e2e-tests/named.spec.ts
@@ -1,22 +1,11 @@
 import { test } from "@playwright/test";
 import { goto, isDeployed, testDates } from "./utils";
 
-test("has ISR pages (static override)", async ({ page }) => {
+test("has dynamic pages", async ({ page }) => {
   test.skip(!isDeployed);
-  test.slow();
   await goto(page, "/named/id-1");
   await testDates(page, "newer");
 
   await goto(page, "/named/id-2");
-  await testDates(page, "newer");
-});
-
-test("has ISR pages", async ({ page }) => {
-  test.skip(!isDeployed);
-  test.slow();
-  await goto(page, "/named/id-3");
-  await testDates(page, "newer");
-
-  await goto(page, "/named/something");
   await testDates(page, "newer");
 });
