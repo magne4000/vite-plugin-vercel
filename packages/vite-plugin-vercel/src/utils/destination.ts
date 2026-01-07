51052d6dc93d053c686d3d2e58c07b2aee764536
Joël Charles <joel.charles91@gmail.com>
2026-01-07 09:51:11 +0100

    chore

--- packages/vite-plugin-vercel/src/utils/destination.ts
+++ packages/vite-plugin-vercel/src/utils/destination.ts
@@ -1,31 +1,11 @@
-import { createHash } from "node:crypto";
 import path from "node:path";
 import type { EntryMeta } from "@universal-deploy/store";
 import { removeExtension } from "./extension";
 import { pathRelativeTo } from "./path";
 
-function shortHash(obj: unknown) {
-  const str = JSON.stringify(obj, (_key, val) => {
-    // URLPattern always stringifies as `{}` by default
-    // @ts-expect-error
-    if (typeof URLPattern !== "undefined" && val instanceof URLPattern) {
-      return {
-        pathname: val.pathname,
-        search: val.search,
-        hash: val.hash,
-        protocol: val.protocol,
-        hostname: val.hostname,
-      };
-    }
-    return val;
-  });
-
-  return createHash("sha256").update(str).digest("hex").slice(0, 7);
-}
-
 export function entryDestinationDefault(root: string, entry: EntryMeta) {
   const rel = pathRelativeTo(entry.id, root);
-  return `${removeExtension(rel).replace(/[^a-zA-Z0-9\-_[\]/]/g, "-")}-${shortHash(entry)}`;
+  return `${removeExtension(rel).replace(/[^a-zA-Z0-9\-_[\]/]/g, "-")}`;
 }
 
 export function entryDestination(
