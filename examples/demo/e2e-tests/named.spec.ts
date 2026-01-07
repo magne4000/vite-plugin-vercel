dcb4e15301356ac76f4cc773c3aadeaa682d002b
Joël Charles <joel.charles91@gmail.com>
2026-01-07 10:52:56 +0100

    test

--- examples/demo/e2e-tests/named.spec.ts
+++ examples/demo/e2e-tests/named.spec.ts
@@ -1,36 +1,22 @@
-import { expect, test } from "@playwright/test";
-import { goto, isDeployed, testISRDates } from "./utils";
+import { test } from "@playwright/test";
+import { goto, isDeployed, testDates } from "./utils";
 
 test("has ISR pages (static override)", async ({ page }) => {
   test.skip(!isDeployed);
   test.slow();
   await goto(page, "/named/id-1");
-  await testISRDates(page, 10000);
+  await testDates(page, "newer");
 
   await goto(page, "/named/id-2");
-  await testISRDates(page, 10000);
+  await testDates(page, "newer");
 });
 
 test("has ISR pages", async ({ page }) => {
   test.skip(!isDeployed);
   test.slow();
   await goto(page, "/named/id-3");
-  await testISRDates(page, 10000);
+  await testDates(page, "newer");
 
   await goto(page, "/named/something");
-  await testISRDates(page, 10000);
-});
-
-test("has 404 pages", async ({ page }) => {
-  test.skip(!isDeployed);
-  test.slow();
-  {
-    const response = await goto(page, "/named/id-1/a");
-    expect(response?.status()).toBe(404);
-  }
-
-  {
-    const response = await goto(page, "/named/something/a");
-    expect(response?.status()).toBe(404);
-  }
+  await testDates(page, "newer");
 });
